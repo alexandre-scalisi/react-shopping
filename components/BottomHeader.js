b1f0d34bccf690e314bc28728cf24347814e60a0
alexlemia13 <alexlemia13@outlook.fr>
2021-12-14 01:16:20 +0100

    fait style bottom header

---
+++ components/BottomHeader.js
@@ -0,0 +1,47 @@
+import NextLink from "next/link";
+
+import classes from "./BottomHeader.module.css";
+
+const BottomHeader = () => {
+  return (
+    <nav className={classes.bottomHeader}>
+      <NextLink href="/all">
+        <a className={classes.link}>Tout</a>
+      </NextLink>
+      <NextLink href="/latest">
+        <a className={classes.link}>Dernier produits</a>
+      </NextLink>
+      <NextLink href="/best-sellers">
+        <a className={classes.link}>Tout</a>
+      </NextLink>
+      <NextLink href="/category1">
+        <a className={classes.link}>Categorie 1</a>
+      </NextLink>
+      <NextLink href="/category2">
+        <a className={classes.link}>Categorie 2</a>
+      </NextLink>
+      <NextLink href="/category3">
+        <a className={classes.link}>Categorie 3</a>
+      </NextLink>
+      <NextLink href="/category4">
+        <a className={classes.link}>Categorie 4</a>
+      </NextLink>
+      <select
+        className={classes.dropdown}
+        name="allCategories"
+        id="allCategories"
+        onChange={(e) => {
+          router.push(e.target.value);
+        }}
+      >
+        <option value="category1">Categorie 1</option>
+        <option value="category2">Categorie 2</option>
+        <option value="category3">Categorie 3</option>
+        <option value="category4">Categorie 4</option>
+        <option value="category5">Categorie 5</option>
+      </select>
+    </nav>
+  );
+};
+
+export default BottomHeader;
--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,7 @@ import classes from "./Header.module.css";
 import Cart from "./Cart/Cart";
 import SearchBar from "./SearchBar";
 import AuthButton from "./UI/Buttons/AuthButton";
+import BottomHeader from "./BottomHeader";
 
 const Header = () => {
   const router = useRouter();
@@ -29,28 +30,7 @@ const Header = () => {
           </nav>
         </div>
       </div>
-      <nav>
-        <NextLink href="/all">Tout</NextLink>
-        <NextLink href="/latest">Dernier produits</NextLink>
-        <NextLink href="/best-sellers">Tout</NextLink>
-        <NextLink href="/category1">Categorie 1</NextLink>
-        <NextLink href="/category2">Categorie 2</NextLink>
-        <NextLink href="/category3">Categorie 3</NextLink>
-        <NextLink href="/category4">Categorie 4</NextLink>
-        <select
-          name="allCategories"
-          id="allCategories"
-          onChange={(e) => {
-            router.push(e.target.value);
-          }}
-        >
-          <option value="category1">categorie 1</option>
-          <option value="category2">categorie 2</option>
-          <option value="category3">category3</option>
-          <option value="category4">category4</option>
-          <option value="category5">category5</option>
-        </select>
-      </nav>
+      <BottomHeader />
     </header>
   );
 };
